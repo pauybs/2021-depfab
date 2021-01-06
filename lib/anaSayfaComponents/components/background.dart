60b5916387a558fdb22c6c1c2d5621c31ac0ea96
YkpAtas <ykp.atas@outlook.com>
2021-01-06 13:47:32 -0500

    Location eklendi

--- lib/anaSayfaComponents/components/background.dart
+++ lib/anaSayfaComponents/components/background.dart
@@ -1,42 +1,102 @@
-import 'dart:async';
+
+import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 import 'package:login/anaSayfaComponents/components/drawler.dart';
 
-MapType _defaultMapType = MapType.terrain;
-Completer<GoogleMapController> _controller = Completer();
+
+
 
 class AnaSayfaBackground extends StatefulWidget {
+    final LocationData location;
+    AnaSayfaBackground({this.location});
   @override
   _BacgroundState createState() => _BacgroundState();
 }
 
 class _BacgroundState extends State<AnaSayfaBackground> {
   GoogleMapController mapController;
-
-  final LatLng _center = const LatLng(37.7852516, 29.0910065);
-
+  bool _serviceEnabled;
+  Location location=new Location();
+  PermissionStatus _permisionGranted;
+  LocationData _locationData;
+ int _markerIdCounter=1;
+   Set<Marker>_isaretler=HashSet<Marker>();
+  bool _isMarker=false;
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text(
-              "EarthQuege",
-            ),
-            centerTitle: true,
-          ),
-          drawer: Menu(),
-          body: GoogleMap(
-            onMapCreated: _onMapCreated,
-            myLocationEnabled: true,
-            mapType: _defaultMapType,
-            initialCameraPosition: CameraPosition(target: _center, zoom: 11.0),
-          )),
-    );
+  void _setMerkers(LatLng point){
+    final String markerIdVal ='marker_id_$_markerIdCounter';
+    _markerIdCounter++;
+    setState(() {
+      print('Marker | Latitude: ${point.latitude} Longitude: ${point.longitude}');
+      _isaretler.add(
+        Marker(markerId: MarkerId(markerIdVal),
+        position: point)
+      );
+    });
   }
-}
+  @override
+  void initState(){
+    super.initState();
+    _checkLocationPermission();
+    _locationData=widget.location;
+      }
+      
+   void _checkLocationPermission() async {
+      _serviceEnabled=await location.serviceEnabled();
+      if(!_serviceEnabled){
+        _serviceEnabled=await location.requestService();
+        if(!_serviceEnabled){
+          return;
+        }
+      }
+      _permisionGranted=await location.hasPermission();
+      if( _permisionGranted==PermissionStatus.denied){
+        _permisionGranted = await location.requestPermission();
+        if(_permisionGranted!=PermissionStatus.granted){
+          return;
+        }
+      }
+      _locationData=await location.getLocation();
+    }
+
+      Widget build(BuildContext context) {
+        return MaterialApp(
+          home: Scaffold(
+              appBar: AppBar(
+                title: Text(
+                  "Debfab",
+                ),
+                centerTitle: true,
+              ),
+              drawer: Menu(),
+              body: Stack(
+               children:<Widget>[ GoogleMap(
+                 initialCameraPosition: CameraPosition(target: LatLng(_locationData.latitude,_locationData.longitude),
+                 zoom: 16,),
+                 markers: _isaretler,
+                onMapCreated: _onMapCreated,
+                myLocationEnabled: true,
+                mapType: MapType.hybrid,
+                onTap: (point){
+                  if(_isMarker){
+                    setState(() {
+                      _isaretler.clear();
+                      _setMerkers(point);
+                    });
+                  }
+                },
+               
+              ),
+               ]
+              ),
+               
+          ),
+        );
+      }
+    }
+    
+    
